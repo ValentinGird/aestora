245aa910102878f5d7b4797ded47832e95a85d54
Valentin <valentingird@gmail.com>
2025-02-07 03:50:59 +0100

    Fix build and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aestora/",  // <-- Ajoute cette ligne pour corriger les chemins
+  base: "/aestora/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    manifest: true, // Permet de générer un manifest pour voir les fichiers générés
+    rollupOptions: {
+      input: {
+        main: "src/main.tsx", // Assure-toi que le fichier d’entrée est bien spécifié
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
